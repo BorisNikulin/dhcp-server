91127d7af1467c8fb7820b7b3d0bd181f82eb552
Boris Nikulin <NikulinBE@gmail.com>
2019-03-15 13:41:20 -0700

    Implement double ended singly linked list

---
+++ src/singly_linked_list.py
@@ -0,0 +1,64 @@
+from typing import Optional, TypeVar, Generic, cast, Iterator
+
+T = TypeVar('T')
+
+class Node(Generic[T]):
+    def __init__(self, nextNode: Optional['Node[T]'], data: T):
+        self.nextNode = nextNode
+        self.data = data
+
+    def __iter__(self) -> Iterator[T]:
+        return NodeIter(self)
+
+
+class NodeIter(Generic[T]):
+    def __init__(self, node: Optional[Node[T]]):
+        self.curNode = node
+
+    def __iter__(self) -> Iterator[T]:
+        return self
+
+    def __next__(self) -> T:
+        if self.curNode is not None:
+            data = cast(Node[T], self.curNode).data
+            self.curNode = self.curNode.nextNode
+            return data
+        else:
+            raise StopIteration
+
+class DoubleEndedLinkedList(Generic[T]):
+    def __init__(self):
+        self.head: Optional[Node[T]] = None
+        self.tail: Optional[Node[T]] = None
+
+    def __pushWhenEmpty(self, data: T) -> None:
+        self.head = Node(None, data)
+        self.tail = self.head
+
+    def pushBack(self, data: T) -> None:
+        if self.isEmpty():
+            self.__pushWhenEmpty(data)
+        else:
+            tail = cast(Node[T], self.tail)
+            tail.nextNode = Node(None, data)
+            self.tail = tail.nextNode
+
+    def pushFront(self, data: T) -> None:
+        if self.isEmpty():
+            self.__pushWhenEmpty(data)
+        else:
+            node = Node(self.head, data)
+            self.head = node
+
+    def popFront(self) -> T:
+        """Assumes list is not empty"""
+        head = cast(Node[T], self.head)
+        data = head.data
+        self.head = head.nextNode
+        return data
+
+    def isEmpty(self) -> bool:
+        return self.head is None
+
+    def __iter__(self) -> Iterator[T]:
+        return NodeIter(self.head)
