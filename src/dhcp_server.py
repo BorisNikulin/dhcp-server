b4d1d450901b3ab9025d353e41067d8f3b7653be
Boris Nikulin <NikulinBE@gmail.com>
2019-03-15 13:43:37 -0700

    Update DhcpServer with linked list and sketched out its uses

--- src/dhcp_server.py
+++ src/dhcp_server.py
@@ -1,26 +1,27 @@
-from dhcp_packet import DhcpPacket
+from dhcp_packet import DhcpPacket, MessageType
 from dhcp_server_transaction import ServerTransaction, TransactionType
+from singly_linked_list import DoubleEndedLinkedList
 
-from typing import MutableMapping, Optional
-
+from typing import MutableMapping, Optional, Tuple
 import time
 
 Seconds = float
 
 DEFAULT_LEASE_TIME: Seconds = 30
-
-class Node:
-    def __init__(self, nextNode: Optional[Node], ip: int, leaseTimeOffset: Seconds):
-        self.next = nextNode
-        self.ip = ip
-        self.leaseTimeOffset = leaseTimeOffset
+DEFAULT_TRANSACTION_TIMEOUT: Seconds = 30
 
 class DhcpServer:
     def __init__(self, netId: int):
-        self.__startTime: Seconds = time.time() # note time here is a float and in the packet it's an unsigned int
-        self.__leasedIps: MutableMapping[int, int] = {} # map from ips to mac addresses
-        self.__closestLeaseHead: Optional[Node] = None # a singly linked list for keeping track of the ip closest to expiring
-        self.__curTransactions: MutableMapping[int, ServerTransaction] = {} # map from transaction ids to ServerTransactions
+        # note time here is a float and in the packet it's an unsigned int
+        self.__startTime: Seconds = time.time()
+        # map from mac addresses to ip
+        self.__leasedIps: MutableMapping[int, int] = {}
+        # singly linked list head for mac with ip closest to expiring
+        self.__closestLeaseHead: DoubleEndedLinkedList[Tuple[int, int]] = DoubleEndedLinkedList()
+        # map from transaction ids to ServerTransactions
+        self.__curTransactions: MutableMapping[int, ServerTransaction] = {}
+        # singly linked list of transaction by closest timeout
+        self.__transactionsByTimeoutHead: DoubleEndedLinkedList[Tuple[ServerTransaction, Seconds]] = DoubleEndedLinkedList()
         self.netId: int = netId
         self.__nextIp: int = 1
 
@@ -28,13 +29,15 @@ class DhcpServer:
     def recv(self, packet: DhcpPacket) -> DhcpPacket:
         transaction: ServerTransaction
         if packet.transactionId not in self.__curTransactions:
-            transaction = ServerTransaction(packet)
-            transaction.yourIp = self.__nextIp
-            transaction.leaseTime = int(DEFAULT_LEASE_TIME)
+            if packet.messageType == MessageType.DISCOVER:
+                if packet.clientHardwareAddr not in self.__leasedIps:
+                    transaction = ServerTransaction(packet)
+                    transaction.yourIp = self.__nextIp
+                    transaction.leaseTime = int(DEFAULT_LEASE_TIME)
 
-            self.__curTransactions[packet.transactionId] = transaction
-            self.__freeIps()
-            self.__setNextIp()
+                    self.__curTransactions[packet.transactionId] = transaction
+                    self.__freeIps()
+                    self.__setNextIp()
         else:
             transaction = self.__curTransactions[packet.transactionId]
 
@@ -42,6 +45,12 @@ class DhcpServer:
         if returnPacket is not None:
             self.__leaseIp(transaction.yourIp, transaction.clientHardwareAddr)
         return returnPacket
+
+    def registerTransaction(self, transaction: ServerTransaction):
+        now: Seconds = time.time()
+        timeout = now + DEFAULT_TRANSACTION_TIMEOUT - self.__startTime
+
+
 
     def __leaseIp(self, ip: int, clientHardwareAddr: int) -> None:
         pass
