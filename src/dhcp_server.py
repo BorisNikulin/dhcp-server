4e93576a8e6a928f9888bca5422a2186d914e056
Boris Nikulin <NikulinBE@gmail.com>
2019-03-15 21:44:37 -0700

    Flesh out DhcpServer more with stubs and more accurate types

--- src/dhcp_server.py
+++ src/dhcp_server.py
@@ -14,33 +14,34 @@ class DhcpServer:
     """Class for handling all DHCP packets and replying appropriately."""
 
     def __init__(self, netId: int):
-        # note time here is a float and in the packet it's an unsigned int
-        self.__startTime: Seconds = time.time()
         # map from mac addresses to ip
         self.__leasedIps: MutableMapping[int, int] = {}
-        # singly linked list head for mac with ip closest to expiring
-        self.__closestLeaseHead: DoubleEndedLinkedList[Tuple[int, int]] = DoubleEndedLinkedList()
+        # singly linked list head for time of timeout and macs sorted by ascending time of timeout
+        self.__macsByClosestLeases: DoubleEndedLinkedList[Tuple[Seconds, int]] = DoubleEndedLinkedList()
         # map from transaction ids to ServerTransactions
         self.__curTransactions: MutableMapping[int, ServerTransaction] = {}
         # singly linked list of transaction by closest timeout
-        self.__transactionsByTimeoutHead: DoubleEndedLinkedList[Tuple[ServerTransaction, Seconds]] = DoubleEndedLinkedList()
+        self.__transactionsByTimeouts: DoubleEndedLinkedList[Tuple[Seconds, ServerTransaction]] = DoubleEndedLinkedList()
         self.netId: int = netId
-        self.__nextIp: int = 1
+        self.__nextIp: Optional[int] = 1
 
-    def recv(self, packet: DhcpPacket) -> DhcpPacket:
+    def recv(self, packet: DhcpPacket) -> Optional[DhcpPacket]:
         """Recieves a DHCP packet and returns a response packet.
 
         The response packet may be None to indicate to not reply."""
+
+        self.__timeoutTransactions()
 
         transaction: ServerTransaction
         if packet.transactionId not in self.__curTransactions:
             if packet.messageType == MessageType.DISCOVER:
                 if packet.clientHardwareAddr not in self.__leasedIps:
-                    transaction = ServerTransaction(packet)
-                    transaction.yourIp = self.__nextIp
-                    transaction.leaseTime = int(DEFAULT_LEASE_TIME)
+                    if self.__nextIp is not None:
+                        transaction = ServerTransaction(packet)
+                        transaction.yourIp = self.__nextIp
+                        transaction.leaseTime = int(DEFAULT_LEASE_TIME)
+                        self.__registerTransaction(transaction)
 
-                    self.__curTransactions[packet.transactionId] = transaction
                     self.__freeIps()
                     self.__setNextIp()
         else:
@@ -51,11 +52,16 @@ class DhcpServer:
             self.__leaseIp(transaction.yourIp, transaction.clientHardwareAddr)
         return returnPacket
 
-    def registerTransaction(self, transaction: ServerTransaction):
+    def __registerTransaction(self, transaction: ServerTransaction):
         """Register a new transaction."""
 
-        now: Seconds = time.time()
-        timeout = now + DEFAULT_TRANSACTION_TIMEOUT - self.__startTime
+        timeout = time.time() + DEFAULT_TRANSACTION_TIMEOUT
+        self.__curTransactions[ServerTransaction.transactionId] = transaction
+        self.__transactionsByTimeouts.pushBack((timeout, transaction))
+
+    def __timeoutTransactions(self) -> None:
+        """Drop any transactions whose transaction has not completed in a set period."""
+        pass
 
     def __leaseIp(self, ip: int, clientHardwareAddr: int) -> None:
         """Reserve an IP address on the server."""
