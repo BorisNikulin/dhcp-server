2a14d9cc1bdbc4acd24d1790b2572dbba53b7337
Boris Nikulin <NikulinBE@gmail.com>
2019-03-15 19:07:35 -0700

    Modify DhcpPacket to allow for a variable number of options
    Options supported are:
      * 0: pad byte (ignored)
      * 51: lease time
      * 255: option stop byte
      * all other options are ignored
    
    Note that option 53, or message type, is expected as the first option
    and for the option to be present in all messages.
    A non option optional if you will.

--- src/dhcp_packet.py
+++ src/dhcp_packet.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from enum import Enum
 from struct import Struct
 
@@ -15,35 +16,74 @@ class MessageType(Enum):
     RELEASE = 7
     INFORM = 8
 
+class DhcpPartialPacket:
+    def __init__(self, initialBytes: bytes):
+        self.packet: DhcpPacket = DhcpPacket()
+
+        unpacked = DhcpPacket.codec.unpack(initialBytes)
+        self.packet.opCode = unpacked[0]
+        self.packet.transactionId = unpacked[4]
+        self.packet.secondsElapsed = unpacked[5]
+        self.packet.clientIp = unpacked[7]
+        self.packet.yourIp = unpacked[8]
+        self.packet.serverIp = unpacked[9]
+        self.packet.clientHardwareAddr = unpacked[11]
+        self.packet.messageType = MessageType(unpacked[20])
+
+        self.__optionType: Optional[int] = unpacked[21]
+        self.__optionLen: Optional[int] = None
+        self.bytesNeeded: int = 0 if self.__optionType == 255 else 1
+
+    def parseMore(self, moreBytes: bytes) -> None:
+        if self.bytesNeeded == 0:
+            raise ValueError('Parsing is complete')
+        elif self.bytesNeeded != len(moreBytes):
+            raise ValueError('Extra bytes given does not correspond with number of bytes needed')
+
+        if self.__optionType is None:
+            self.__optionType = int.from_bytes(moreBytes, 'big', signed=False)
+            if self.__optionType == 255: # end options option
+                self.bytesNeeded = 0
+            else:
+                if self.__optionType == 0: # pad option
+                    self.__optionType = None
+                self.bytesNeeded = 1
+
+        elif self.__optionLen is None:
+            self.__optionLen = int.from_bytes(moreBytes, 'big', signed=False)
+            self.bytesNeeded = self.__optionLen
+
+        else:
+            if self.__optionType == 51:
+                DhcpPacket.leaseTime = int.from_bytes(moreBytes, 'big', signed=False)
+            self.__optionType = None
+            self.__optionLen = None
+            self.bytesNeeded = 1
+
 class DhcpPacket:
     opCode: OpCode
     transactionId: int
-    secondsElapsed: int
+    secondsElapsed: int # unsigned
     clientIp: int
     yourIp: int
     serverIp: int
     clientHardwareAddr: int
     messageType: MessageType
+    leaseTime: Optional[int] # unsigned
+
+    def __init__(self):
+        leaseTime = None
 
     # main packet + optional header and first optional (not really opional) + type of next optional
     # uses big endianness
-    # most of the fields will not be uses for simplicity's sake
+    # most of the fields will not be used for simplicity's sake
     codec = Struct('>4BI2H4IQ64s128s' + '4B3B' + 'B')
-    optionHeaderDhcpMagic = [0x63, 0x82, 0x53, 0x63]
+    __optionHeaderDhcpMagic = [0x63, 0x82, 0x53, 0x63]
+    initialPacketSize = codec.size
 
     @staticmethod
-    def fromPacket(packet: bytes) -> DhcpPacket:
-        packetObj = DhcpPacket()
-        unpacked = DhcpPacket.codec.unpack(packet)
-        packetObj.opCode = unpacked[0]
-        packetObj.transactionId = unpacked[4]
-        packetObj.secondsElapsed = unpacked[5]
-        packetObj.clientIp = unpacked[7]
-        packetObj.yourIp = unpacked[8]
-        packetObj.serverIp = unpacked[9]
-        packetObj.clientHardwareAddr = unpacked[11]
-        packetObj.messageType = MessageType(unpacked[20])
-        return packetObj
+    def fromPacket(initialBytes: bytes) -> DhcpPartialPacket:
+        return DhcpPartialPacket(initialBytes)
 
     @staticmethod
     def fromArgs(
@@ -54,7 +94,9 @@ class DhcpPacket:
             yourIp: int,
             serverIp: int,
             clientHardwareAddr: int,
-            messageType: MessageType) -> DhcpPacket:
+            messageType: MessageType,
+            leaseTime: Optional[int] = None
+        ) -> 'DhcpPacket':
         packetObj = DhcpPacket()
         packetObj.opCode = opCode
         packetObj.transactionId = transactionId
@@ -64,9 +106,16 @@ class DhcpPacket:
         packetObj.serverIp = serverIp
         packetObj.clientHardwareAddr = clientHardwareAddr
         packetObj.messageType = messageType
+        packetObj.leaseTime = leaseTime
         return packetObj
 
     def encode(self) -> bytes:
+        lastType: int = 255
+        extraBytes: bytes = b''
+        if self.leaseTime is not None:
+            lastType = 51
+            extraBytes = bytes([4]) + self.leaseTime.to_bytes(4, 'big') + bytes([255])
+
         return DhcpPacket.codec.pack(
             self.opCode.value,
             1, # ethernet hardware type
@@ -82,8 +131,8 @@ class DhcpPacket:
             self.clientHardwareAddr,
             b'', # servername
             b'', # boot filename
-            *DhcpPacket.optionHeaderDhcpMagic,
+            *DhcpPacket.__optionHeaderDhcpMagic,
             53,
             1,
             self.messageType.value,
-            255)
+            lastType) + extraBytes
