45ef190d033fd5cafa6b52eca50407daf5e3e977
Erik Anderson <anderikson1@gmail.com>
2019-03-16 18:56:27 -0700

    Update dhcp_client_transaction.py
    Worked on client transaction class

--- src/dhcp_client_transaction.py
+++ src/dhcp_client_transaction.py
@@ -4,9 +4,11 @@ from dhcp_packet import DhcpPacket
 from typing import Optional
 from enum import Enum
 
+DEFAULT_LEASE_TIME: Seconds = 30
+
 class ClientTransaction(Transaction):
     """Class for representing an onogoing transaction with the server."""
-
+    
     # start a transaction
     # returns a DhcpPacket to send to the server
     def start(self, transactionType: TransactionType) -> DhcpPacket:
@@ -16,6 +18,15 @@ class ClientTransaction(Transaction):
         """
 
         self.transactionType = transactionType
+        return DhcpPacket.fromArgs(
+            1,
+            random.getrandbits(),
+            0,
+            0,
+            0,
+            255,
+            
+            )
 
     # recieve a packet from server
     # returns a DhcpPacket to respond with or None to conclude the transaction
@@ -25,3 +36,25 @@ class ClientTransaction(Transaction):
         The response packet may be None to indicate conclusion of the session.
         """
         self._phase += 1
+
+        if packet.messageType == MessageType.OFFER:
+
+            return DhcpPacket.fromArgs(
+                1, #opCode
+                packet.transactionId,
+                packet.secondsElapsed,
+                0, #clientIP
+                packet.yourIP,
+                packet.serverIp,
+                packet.clientHardwareAddr,
+                MessageType.REQUEST,
+                DEFAULT_LEASE_TIME
+                )
+        if packet.messageType == MessageType.DECLINE:
+            print("REQUEST DECLINED")
+            return None
+
+        if packet.messageType == MessageType.ACK:
+            return None
+
+        
