f55ddde92e8b8b392e653a6300893825c3c01e58
Boris Nikulin <NikulinBE@gmail.com>
2019-03-20 16:27:00 -0700

    Change ServerTransaction to use a generator as a coroutine

--- src/dhcp_server_transaction.py
+++ src/dhcp_server_transaction.py
@@ -1,7 +1,7 @@
 from dhcp_packet import DhcpPacket, MessageType, OpCode
 from dhcp_transaction import Transaction, TransactionType
 
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Generator
 
 class ServerTransaction(Transaction):
     """Class for representing an ongoing session with a client.
@@ -13,62 +13,57 @@ class ServerTransaction(Transaction):
     This class forms a tagged union or sum type with the self.transactionType being the tag.
     """
 
-    yourIp: Optional[int]
+    __transaction: Optional[Generator[DhcpPacket, DhcpPacket, DhcpPacket]]
+
+    yourIp: int
     serverIp: int
     leaseTime: int # unsigned
 
-    # recieve a packet from client
-    # returns a DhcpPacket to respond with or None to conclude the transaction
-    def recv(self, packet: DhcpPacket) -> Tuple[bool, Optional[DhcpPacket]]:
+    def __init__(self):
+        self.__transaction = None
 
-        self._phase += 1
-        if self._phase == 1:
-            self.transactionId = packet.transactionId
-            self.clientHardwareAddr = packet.clientHardwareAddr
+    def __gen(self, packet: DhcpPacket) -> Generator[DhcpPacket, DhcpPacket, DhcpPacket]:
+        self.transactionId = packet.transactionId
+        self.clientHardwareAddr = packet.clientHardwareAddr
 
-            if packet.messageType is MessageType.DISCOVER:
-                self.transactionType = TransactionType.RENEW
-                return (True, DhcpPacket.fromArgs(
+        if packet.messageType is MessageType.DISCOVER:
+            self.transactionType = TransactionType.RENEW
+            packet = yield DhcpPacket.fromArgs(
+                OpCode.REPLY,
+                self.transactionId,
+                packet.secondsElapsed,
+                packet.clientIp,
+                self.yourIp,
+                0, # TODO: figure out what to do with serverIp (nothing?)
+                packet.clientHardwareAddr,
+                MessageType.OFFER,
+                self.leaseTime)
+
+            if packet.messageType is MessageType.REQUEST:
+                return DhcpPacket.fromArgs(
                     OpCode.REPLY,
                     self.transactionId,
                     packet.secondsElapsed,
                     packet.clientIp,
-                    0 if self.yourIp is None else self.yourIp,
-                    0, # TODO: figure out what to do with serverIp (nothing?)
+                    self.yourIp,
+                    0, # TODO: server ip? see todo above
                     packet.clientHardwareAddr,
-                    MessageType.OFFER,
-                    self.leaseTime))
-
-            elif packet.messageType is MessageType.RELEASE:
-                self.transactionType = TransactionType.RELEASE
-                raise NotImplementedError
-
+                    MessageType.ACK,
+                    self.leaseTime)
             else:
-                raise ValueError('Unsuported transaction')
-
-        elif self._phase == 2:
-            if self.transactionType is TransactionType.RENEW:
-                if packet.messageType is MessageType.REQUEST:
-                    return (False, DhcpPacket.fromArgs(
-                        OpCode.REPLY,
-                        self.transactionId,
-                        packet.secondsElapsed,
-                        packet.clientIp,
-                        0 if self.yourIp is None else self.yourIp,
-                        0, # TODO: server ip? see todo above
-                        packet.clientHardwareAddr,
-                        MessageType.ACK,
-                        self.leaseTime))
-
-                else:
-                    raise ValueError(f'Expecting DHCP request. Got {packet.messageType.name}')
-            else:
-                if packet.messageType is MessageType.ACK:
-                    raise NotImplementedError
-
-                else:
-                    raise ValueError(f'Expecting DHCP ack. Got {packet.messageType.name}')
+                raise ValueError(f'Expected DHCP REQUEST. Got {packet.messageType.name}.')
 
         else:
-            raise ValueError('recieved too many packets')
+            raise ValueError('Unsuported transaction')
 
+    # recieve a packet from client
+    # returns a DhcpPacket to respond with or None to conclude the transaction
+    def recv(self, packet: DhcpPacket) -> Tuple[bool, Optional[DhcpPacket]]:
+        try:
+            if self.__transaction is None:
+                self.__transaction = self.__gen(packet)
+                return (True, next(self.__transaction))
+            else:
+                return (True, self.__transaction.send(packet))
+        except StopIteration as ex:
+            return (False, ex.value)
