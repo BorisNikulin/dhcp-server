758de7d9180fa09cf80f8c86cb141ba3e6afcd97
Boris Nikulin <NikulinBE@gmail.com>
2019-03-25 08:57:15 -0700

    Initial implementation of dhcp server application

---
+++ dhcpserver.py
@@ -0,0 +1,43 @@
+from dhcp.server import DhcpServer
+from dhcp.packet import DhcpPacket
+
+import logging
+import socket
+from ipaddress import IPv4Address, IPv4Interface
+
+SERVER_PORT = 4200
+SERVER_INTERFACE = IPv4Interface('192.168.0.255/24')
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(levelname)s: %(name)s - %(message)s')
+
+log = logging.getLogger(__name__)
+
+
+def parsePacket(packet: bytes) -> DhcpPacket:
+    partialPacket = DhcpPacket.fromPacket(
+        packet[:DhcpPacket.initialPacketSize])
+    offset: int = DhcpPacket.initialPacketSize
+    while partialPacket.bytesNeeded > 0:
+        nextOffset = offset + partialPacket.bytesNeeded
+        partialPacket.parseMore(
+            packet[offset:offset + partialPacket.bytesNeeded])
+        offset = nextOffset
+
+    return partialPacket.packet
+
+
+if __name__ == '__main__':
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serverSocket.bind(('', SERVER_PORT))
+    log.info(f'Server bound to port {SERVER_PORT}')
+    server = DhcpServer(SERVER_INTERFACE)
+
+    while True:
+        packetBytes = serverSocket.recv(4096)
+        packet = parsePacket(packetBytes)
+        response = server.recv(packet)
+        if response is not None:
+            serverSocket.sendto(
+                response.encode(), ('', int(IPv4Address('255.255.255.255'))))
